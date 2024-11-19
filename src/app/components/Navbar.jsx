173a2326c427b4a23150e8b5c3d6f93159577b94
Abstractonion <calebuzu@gmail.com>
2024-11-19 15:24:18 +0100

    feat

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,11 +1,28 @@
+import Link from "next/link"
+
 export default function Navbar() {
   return (
     <nav>
-        <ul>
+        <ul className="flex flex-row justify-between items-center bg-blue-700 text-white h-28 font-bold capitalize px-4">
+            <Link href="/">
             <li>Home</li>
+            </Link>
+
+            <Link href="/about">
             <li>About</li>
+            </Link>
+
+            <Link href="/categories">
             <li>Categories</li>
-            <li>Create an Account</li>
+            </Link>
+
+            <Link href="/contact">
+            <li>Contact</li>
+            </Link>
+
+            <Link href="/sign-up">
+            <li>Create Account</li>
+            </Link>
         </ul>
     </nav>
   )
