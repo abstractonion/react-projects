594b6f85cece6632dac59bb2c91741ac23e5e54f
Abstractonion <calebuzu@gmail.com>
2024-11-21 15:26:00 +0100

    feat

--- src/app/shop/page.jsx
+++ src/app/shop/page.jsx
@@ -1,16 +1,16 @@
 import {products} from "../components/data"
-import king from "../components/product.module.css"
+import styles from "../components/product.module.css"
 
 export default function ShopPage() {
     const productCat = products.map(product => (
-        <section key={product.id} className="">
+        <section key={product.id} className="flex flex-col items-left hover:shadow-3xl">
             <p>{product.id}</p>
             <h2>{product.name}</h2>
             <p>{product.brand}</p> 
         </section>
     ))
     return (
-        <div className={king.product}>
+        <div className={styles.product}>
             {productCat}
         </div>
     )
