0948dac006680aeb425433ee2ab482a9a595fcc7
Abstractonion <calebuzu@gmail.com>
2024-11-28 12:45:38 +0100

    feat

---
+++ src/app/components/Mobile.jsx
@@ -0,0 +1,51 @@
+"use client"
+
+import Link from 'next/link'
+import { useState } from 'react'
+import {GiHamburgerMenu} from 'react-icons/gi'
+
+export default function() {
+    const pageTitle = ["Home"]
+    const [open, setOpen] = useState(false)
+
+    function handleNav(){
+        setOpen(!open)
+    }
+
+  return (
+    <div className="md:hidden">
+        <ul className="flex flex-row justify-evenly bg-blue-700 text-white h-16 font-regular capitalize px-4">
+            <li><Link href={'/'}>{pageTitle}</Link></li>
+            <li onClick={handleNav} className='cursor-pointer'><GiHamburgerMenu/></li>
+        </ul>
+
+        {open &&
+            <ul className="flex flex-col justify-between items-center bg-blue-700 text-white h-16 font-regular capitalize px-4">
+            <Link href="/">
+            <li>Home</li>
+            </Link>
+
+            <Link href="/about">
+            <li>About</li>
+            </Link>
+
+            <Link href="/categories">
+            <li>Categories</li>
+            </Link>
+
+            <Link href="/shop">
+            <li>Shop</li>
+            </Link>
+
+            <Link href="/contact">
+            <li>Contact</li>
+            </Link>
+
+            <Link href="/sign-up">
+            <li>Create Account</li>
+            </Link>
+        </ul>
+        }
+    </div>
+  )
+}
--- src/app/shop/[slug]/page.jsx
+++ src/app/shop/[slug]/page.jsx
@@ -1,17 +1,43 @@
-import React from 'react'
-import {fruits} from '@/app/components/data'
-import Image from 'next/image'
-import Link from 'next/link'
+import React from 'react';
+import { products } from '@/app/components/data';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from '@/app/components/product.module.css'
 
-export default function FruitPage({params}) {
-    const fruitDetails = fruits.find(fruit => fruit.name.split(' ').join('-') == params.slug)
-    
-    const relatedFruits = fruits.filter(fruit => (fruit.category == fruitDetails.category) && (fruitDetails.name !== fruit.name)).map(product =>(
-        <section>
-            
-        </section>
-    ))
-  return (
-    <div>page</div>
-  )
+export default function FruitPage({ params }) {
+    const fruitDetails = products.find(fruit => fruit.name.split(' ').join('-') === params.slug);
+
+    if (!fruitDetails) {
+        return <p>Fruit not found</p>;
+    }
+
+    const relatedFruits = products.filter(fruit => fruit.category === fruitDetails.category && fruit.name !== fruitDetails.name).map(product => (
+            <section key={product.id} className="flex flex-col items-center hover:shadow-3xl">
+                <Link href={`/shop/${product.name.replace(/\s+/g, '-')}`}>
+                    <Image src={product.imageUrl} alt={product.name} width={300} height={150} />
+                    <p>ID: 100{product.id}</p>
+                    <h2>{product.name}</h2>
+                    <p>{product.category}</p>
+                    <p><i>{product.brand}</i></p>
+                </Link>
+            </section>
+        ));
+
+    return (
+        <div>
+            <section>
+                <div className='flex flex-col items-center'>
+                    <h1>Fruit Name: {fruitDetails.name}</h1>
+                    <Image src={fruitDetails.imageUrl} alt={fruitDetails.name} width={300} height={150} />
+                    <p>{fruitDetails.brand}</p>
+                    <p>{fruitDetails.description}</p>
+                    <p>&#8358; {fruitDetails.price}</p>
+                </div>
+
+                <div className={styles.product}>
+                    {relatedFruits}
+                </div>
+            </section>
+        </div>
+    );
 }
