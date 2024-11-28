608f329c015ff16ecc2f26e72ef29156f9130781
Abstractonion <calebuzu@gmail.com>
2024-11-28 15:43:51 +0100

    feat

--- src/app/components/Search.jsx
+++ src/app/components/Search.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Search({holder, title, handle}) {
   return (
-    <div className='border-2 border-blue-100 rounded-2xl w-3/4 my-4 m-auto'>
-        <input type="text" name="search" id="search" className='outline-none w-3/4 p-4' placeholder={holder} defaultValue={title} onChange={handle}/>
+    <div className='border-2 border-blue-50 rounded-full w-3/4 my-4 m-auto'>
+        <input type="text" name="search" id="search" className='outline-none w-3/4 p-4 bg-transparent' placeholder={holder} defaultValue={title} onChange={handle}/>
     </div>
   )
 }
--- src/app/shop/page.jsx
+++ src/app/shop/page.jsx
@@ -1,11 +1,25 @@
+"use client"
 import {products} from "../components/data"
 import styles from "../components/product.module.css"
 import Image from 'next/image'
 import Link from "next/link"
 import Search from "../components/Search"
+import { useState } from "react"
 
 export default function ShopPage() {
-    const productCat = products.map(product => (
+    const [search, setSearch] = useState('')
+
+    function handleSearch(e){
+        setSearch(e.target.value)
+    }
+
+    const filteredProducts = products.filter(product => product.name.toLowerCase().includes(search.toLowerCase()) || product.category.toLowerCase().includes(search.toLowerCase()))
+    
+    const productDetails = filteredProducts.length == 0 ?
+    (
+        <div></div>
+    ) :
+    filteredProducts.map(product => (
         <section key={product.id} className="flex flex-col items-left hover:shadow-3xl">
             <Link href={`/shop/${product.name.replace(' ','-')}`}>
                 <Image src={product.imageUrl} alt={product.name} width={300} height={150}/>
@@ -16,11 +30,23 @@ export default function ShopPage() {
             </Link>
         </section>
     ))
+
+    // const productCat = products.map(product => (
+    //     <section key={product.id} className="flex flex-col items-left hover:shadow-3xl">
+    //         <Link href={`/shop/${product.name.replace(' ','-')}`}>
+    //             <Image src={product.imageUrl} alt={product.name} width={300} height={150}/>
+    //             <p>ID: 100{product.id}</p>
+    //             <h2>{product.name}</h2>
+    //             <p>{product.category}</p>
+    //             <p><i>{product.brand}</i></p>
+    //         </Link>
+    //     </section>
+    // ))
     return (
         <div>
-            <Search/>
+            <Search holder="Search products, brands, categories" title={search} handle={(e)=>handleSearch(e)}/>
             <div className={styles.product}>
-                {productCat}
+                {productDetails}
             </div>
         </div>
     )
